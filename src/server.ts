e094f3a3a0f9890fa7ac1acfc07bdc285c51b206
Milo Rue <milorue@gmail.com>
2021-01-17 16:57:59 -0500

    Added playground in production

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const runServer = async() => {
         const server = new ApolloServer({
             schema,
             context: SetContext,
-            playground: DEV,
+            playground: true,
             cors: {
                 credentials: true,
                 origin: "*",
