69e43da45c6c2a4de88cacde83ee4cf0a0dbdbf0
Milo Rue <milorue@gmail.com>
2022-05-26 20:36:10 -0700

    fix: today prices

--- src/entities/CardPrice.entity.ts
+++ src/entities/CardPrice.entity.ts
@@ -1,45 +1,42 @@
 import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
 import { ObjectType, Field, Float } from "type-graphql";
 
-@Entity({name: "card-prices"})
+@Entity({ name: "card-prices" })
 @ObjectType("CardPrices")
 export class CardPriceEntity extends BaseEntity {
+  @Field(() => String, { nullable: true })
+  @PrimaryGeneratedColumn()
+  id: string;
 
-    @Field(() => String, {nullable: true})
-    @PrimaryGeneratedColumn()
-    id: string;
+  @Field(() => String)
+  @Column()
+  uuid: string;
 
-    @Field(() => String)
-    @Column()
-    uuid: string;
+  @Field(() => String)
+  @Column()
+  format: string;
 
-    @Field(() => String)
-    @Column()
-    format: string;
+  @Field(() => String)
+  @Column()
+  provider: string;
 
-    @Field(() => String)
-    @Column()
-    provider: string;
+  @Field(() => String, { nullable: true })
+  @Column({ nullable: true })
+  currency: string;
 
-    @Field(() => String)
-    @Column()
-    currency: string;
+  @Field(() => String)
+  @Column()
+  date: string;
 
-    @Field(() => String)
-    @Column()
-    date: string
+  @Field(() => String, { description: "Card types can be foil & normal" })
+  @Column()
+  cardType: string;
 
-    @Field(() => String, {description: "Card types can be foil & normal"})
-    @Column()
-    cardType: string
+  @Field(() => String, { description: "Buylist or Retail" })
+  @Column()
+  listType: string;
 
-    @Field(() => String, {description: "Buylist or Retail"})
-    @Column()
-    listType: string;
-
-    @Field()
-    @Column({nullable: true, type: "real"})
-    price: number;
-
-    
+  @Field()
+  @Column({ nullable: true, type: "real" })
+  price: number;
 }
